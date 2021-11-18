0d2cb8cdccec67da92317216f350e0705551eb98
hussamkhatib <hussamkhatib20@gmail.com>
2021-11-18 16:38:59 +0530

    feat: add celo logo in title

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,26 +2,27 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable camelcase */
 import React, { useEffect } from 'react';
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { useUser } from '../stores/user.store'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useUser } from '../stores/user.store';
 import CreateUser from './CreateUser';
+import Celo from './icons/Celo';
 
 function NavBar() {
-  const router = useRouter()
-  const address = useUser(state => state.address)
-  const isRegisteredUser = useUser(state => state.isRegisteredUser)
-  const connectCeloWallet = useUser(state => state.connectCeloWallet)
-  
-  useEffect(()=> {
-    connectCeloWallet()
-  },[])
-  
+  const router = useRouter();
+  const address = useUser((state) => state.address);
+  const isRegisteredUser = useUser((state) => state.isRegisteredUser);
+  const connectCeloWallet = useUser((state) => state.connectCeloWallet);
+
+  useEffect(() => {
+    connectCeloWallet();
+  }, []);
+
   return (
     <>
       <nav className="navbar mb-2 shadow-lg bg-neutral text-neutral-content ">
         <div className="flex-none sm:hidden">
-          <button type='button' className="btn btn-square btn-ghost">
+          <button type="button" className="btn btn-square btn-ghost">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -38,32 +39,36 @@ function NavBar() {
           </button>
         </div>
         <div className="flex-1 px-2 mx-2">
-          <Link href='/'>
+          <Link href="/">
             <a>
-              <h1 className="text-lg font-bold mx-2">Buy me some Moola</h1>
+              <h1 className="text-2xl font-bold mx-2">
+                Buymesomem
+                <Celo size={16} />
+                la
+              </h1>
             </a>
           </Link>
-          <Link href='/explore-creators'>
-            <a className="ml-6">
-              Explore creators
-            </a>
+          <Link href="/explore-creators">
+            <a className="ml-6">Explore creators</a>
           </Link>
         </div>
-        {address && router.pathname === '/'?
-         <Link href='/dashboard'>
-            <a>
-              Go to Dashboard
-            </a>
+        {address && router.pathname === '/' ? (
+          <Link href="/dashboard">
+            <a>Go to Dashboard</a>
           </Link>
-         :  address ?
-        null :
-            <div className="flex-none">
-              <button onClick={connectCeloWallet} type='button' className="btn btn-square btn-ghost">connect</button>
-            </div> 
-        }
-
+        ) : address ? null : (
+          <div className="flex-none">
+            <button
+              onClick={connectCeloWallet}
+              type="button"
+              className="btn btn-square btn-ghost"
+            >
+              connect
+            </button>
+          </div>
+        )}
       </nav>
-      {isRegisteredUser? <CreateUser /> : null}
+      {isRegisteredUser ? <CreateUser /> : null}
     </>
   );
 }
