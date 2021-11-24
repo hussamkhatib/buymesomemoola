8ec40432cb8939165c7c23318c32f7d0050f6dec
hussamkhatib <hussamkhatib20@gmail.com>
2021-11-24 16:33:11 +0530

    refactor: remove useraddress from home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { useUser } from '../src/stores/user.store';
-import Hero from '../src/components/home/Hero'
+import Hero from '../src/components/home/Hero';
 
 export default function HomePage() {
-  const address = useUser((state) => state.address);
-  const error = useUser((state) => state.error);
-
   return (
     <div>
       <Hero />
-      <p>user address {address || 'not connected yet'}</p>
-      {error}
     </div>
   );
 }
