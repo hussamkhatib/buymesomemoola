4eb3cb645880780611401a8d23c456a40ac190bf
hussamkhatib <hussamkhatib20@gmail.com>
2021-11-25 22:23:43 +0530

    feat: add copyToclipboard func

--- src/components/private/dashboard/ShareLink.jsx
+++ src/components/private/dashboard/ShareLink.jsx
@@ -1,22 +1,26 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 function ShareLink({ name }) {
+  const link = `https://buymesomemoola.vercel.app/${name}`;
   return (
     <div className="form-control py-2">
       <div className="relative">
         <input
           type="text"
-          value={`https://buymesomemoola.vercel.app/${name}`}
+          value={link}
           readOnly
           className="w-full pr-16 input input-primary input-bordered bg-neutral"
         />
-        <button
-          type="button"
-          className="absolute top-0 right-0 rounded-l-none btn btn-primary"
-        >
-          Copy
-        </button>
+        <CopyToClipboard text={link}>
+          <button
+            type="button"
+            className="absolute top-0 right-0 rounded-l-none btn btn-primary"
+          >
+            Copy
+          </button>
+        </CopyToClipboard>
       </div>
     </div>
   );
