0b4f7a9cba83055a888aa34727d570e187eb03bb
hussamkhatib <hussamkhatib20@gmail.com>
2021-11-26 19:03:06 +0530

    fix : remove 2k20 in copyrights

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="items-center p-4 footer bg-neutral text-neutral-content">
       <div className="items-center grid-flow-col">
-        <p>© 2020-2021 Buymesomemoola - All Rights Reserved</p>
+        <p>© 2021 Buymesomemoola - All Rights Reserved</p>
       </div>
       <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
         Powered by the Celo network
