1b58ec4780166517c55fb5c51c069c5995160523
hussamkhatib <hussamkhatib20@gmail.com>
2021-11-26 18:32:13 +0530

    fix: check if user is present in db , if not dont allow private routes

--- src/components/RouteGuard.jsx
+++ src/components/RouteGuard.jsx
@@ -5,19 +5,14 @@ import { useUser } from '../stores/user.store';
 
 function RouteGuard({ children }) {
   const address = useUser((state) => state.address);
+  const name = useUser((state) => state.name);
   const isLoading = useUser((state) => state.loading);
-
   const router = useRouter();
   const [authorized, setAuthorized] = useState(false);
 
   useEffect(() => {
-    // on initial load - run auth check
-
-    // on route change start - hide page content by setting authorized to false
-    // const hideContent = () => setAuthorized(false);
-    // router.events.on('routeChangeStart', hideContent);
     const authCheck = () => {
-      if (!address) {
+      if (!address || !name) {
         setAuthorized(false);
         router.push('/');
       } else {
@@ -26,16 +21,9 @@ function RouteGuard({ children }) {
     };
 
     if (!isLoading) authCheck();
-    // on route change complete - run auth check
-    // router.events.on('routeChangeComplete', authCheck)
 
-    // unsubscribe from events in useEffect return function
-    return () => {
-      // router.events.off('routeChangeStart', hideContent);
-      // router.events.off('routeChangeComplete', authCheck);
-    };
+    return () => {};
   }, [address, isLoading]);
-
   return authorized && children;
 }
 
