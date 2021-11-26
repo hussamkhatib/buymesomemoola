0f526f6d9e27c111b36d58061d8c0370cdc7b823
hussamkhatib <hussamkhatib20@gmail.com>
2021-11-26 18:40:23 +0530

    show user address if connected in home page

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useUser } from '../../stores/user.store';
 
 function Hero() {
+  const address = useUser((state) => state.address);
   return (
     <div className="flex flex-col items-center justify-center px-2 h-hero max-w-2xl mx-auto">
       <h3 className="text-secondary-content text-2xl md:text-4xl lg:text-6xl pb-4 md:pb-7 lg:pb-10 font-bold text-center">
@@ -10,15 +12,19 @@ function Hero() {
         Accept donations. Start a membership. Sell anything you like. It’s
         easier than you think.
       </p>
-      <button
-        className="text-secondary-content btn btn-primary w-96 mb-2"
-        type="button"
-      >
-        Start your page
-      </button>
-      <p className="text-secondary-content">
-        It’s free, and takes less than a minute.
-      </p>
+      {address ? (
+        <p className="btn btn-primary w-96 mb-2">{address} connected</p>
+      ) : (
+        <>
+          <button className="btn btn-primary w-96 mb-2" type="button">
+            Connect your wallet to get started
+          </button>
+
+          <p className="text-secondary-content">
+            It’s free, and takes less than a minute.
+          </p>
+        </>
+      )}
     </div>
   );
 }
