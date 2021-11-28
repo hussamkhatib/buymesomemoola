61fcca2d6d7225d6271566e58b75f34ff876849c
hussamkhatib <hussamkhatib20@gmail.com>
2021-11-28 22:47:34 +0530

    render post text using dangerouslyinnerHTML, removes react quill to do it

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,22 +1,21 @@
+/* eslint-disable react/no-danger */
 /* eslint-disable react/prop-types */
 import React from 'react';
 import Image from 'next/image';
-import TextEditorReadOnly from './TextEditorReadOnly';
 
 function Post({ data }) {
   const { title, content } = data;
   return (
-    <div className="max-w-3xl mx-auto w-full py-10">
+    <div className="max-w-3xl mx-auto w-full pt-10 pb-20">
       <div>
         <h1 className="font-bold text-4xl pb-5">{title}</h1>
-
         <div className="relative w-full pb-fifty">
           <Image
             src="https://res.cloudinary.com/dbbunxz2o/image/upload/v1638009094/buymesomemoola/empty-rustic-wooden-table-with-blurred-christmas-lights-at-background-picture-id1041796884_ugnm12.jpg"
             layout="fill"
           />
         </div>
-        <TextEditorReadOnly defaultValue={content} />
+        <div className="pt-6" dangerouslySetInnerHTML={{ __html: content }} />
       </div>
     </div>
   );
